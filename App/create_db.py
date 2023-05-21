c0601c0724bb684f0ed3775dae2b4839ac1f3f1f
Begüm Arslan <begumarslan@outlook.de>
2023-05-21 14:48:47 +0300

    added createtables

--- App/create_db.py
+++ App/create_db.py
@@ -5,14 +5,32 @@ import os
 
 app = Flask(__name__)
 
+# mydb= mysql.connector.connect(
+#     host="127.0.0.1",
+#     user="root",
+#     password="***",
+#     database="sys"
+   
+# )
+
+# print(mydb)
+# mycursor= mydb.cursor()
+# try:
+#     mycursor.execute("CREATE TABLE IF NOT EXISTS DatabaseManagers (username CHAR(20) NOT NULL, password CHAR(20) NOT NULL, PRIMARY KEY (username))")
+
+#     print("Table created successfully.")
+# except mysql.connector.Error as error:
+#     print("Error creating table:", error)
+
+
+
 def execute_sql_file(file_path):
     try:
         connection = mysql.connector.connect(
             host="127.0.0.1",
             user="root",
-            password=process.env.PASSWORD,
-            database="",
-            auth_plugin=""
+            password="****",
+            database="sys"
         )
         cursor = connection.cursor()
 
@@ -31,10 +49,32 @@ def execute_sql_file(file_path):
         if connection:
             connection.close()
 
+def execute_sql_line(line):
+    try:
+        connection = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="Hoplamaz1",
+            database="sys"
+        )
+        cursor = connection.cursor()
+        cursor.execute(line)
+        for x in cursor:
+            print(x)
+        return "SQL file executed successfully"
+    except mysql.connector.Error as error:
+        return "Error executing SQL file: {}".format(error)
+    finally:
+        if connection:
+            connection.close()
+
 @app.route('/')
 def index():
-    result = execute_sql_file("../CreateDatabase/createTables.sql")
+    result = execute_sql_file("../CreateDatabase/create_db_notrigger.sql")
+    result= execute_sql_line("SHOW TABLES")
     return result
 
 if __name__ == '__main__':
+    
     app.run()
+    
